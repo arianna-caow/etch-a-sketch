f194a280d7e6146b4758920660411bfc39a9ce20
Arianna <66382962+arianna-caow@users.noreply.github.com>
2023-08-08 20:26:40 -0400

    fixed borders

--- javascript.js
+++ javascript.js
@@ -1,26 +1,50 @@
+const outerContainer = document.querySelector("#container");
+
 function createGrid(side){
-    const outerContainer = document.querySelector("#container");
     for (let row = 0; row < side; row++) {
         const container = document.createElement("div");
         container.classList.add("row");
         for(let col = 0; col < side; col++){
             const div = document.createElement('div');
-            let size = 80/side;
-            div.style.minWidth= `${size}vh`;
-            div.style.paddingBottom=`${size}vh`;
-            div.style.border="1px ridge"
+            setBorderWidth(div,side);
             div.classList.add('square');
+            div.setAttribute("id","100");
             container.appendChild(div);
         }  
         outerContainer.appendChild(container);
     }
 }
+// 2 w 641px
+// h 544.5px
+// 10 w 641px
+// h 522.656
+// 20 532.5
+
+
+function setBorderWidth(div,side){
+    let size = .8*smallerScreenSize()/side;
+    console.log(size);
+    // let borderWidth = 1/side;
+    // console.log(borderWidth);
+    div.style.minWidth= `${size}px`;
+    div.style.paddingBottom=`${size}px`;
+    // div.style.borderWidth = `${borderWidth}px ${borderWidth}px 0 0`;
+    div.style.border="0";
+}
+function smallerScreenSize(){
+    return (window.innerWidth > window.innerHeight) ? 
+        window.innerHeight
+        :window.innerWidth;
+}
 
 function changeColor(){
     const squares = document.querySelectorAll('.square');
     squares.forEach(square => {
-        square.addEventListener('mouseover', () => {
-            square.style.backgroundColor="#63666A";
+        square.addEventListener('mouseenter', () => {
+            let shade = square.id;
+            shade=(Number(shade)-10);
+            square.setAttribute("id",shade);
+            square.style.backgroundColor=("hsl(0,0%,"+shade+"%)");
         });
     });
 }
@@ -29,10 +53,10 @@ function reset(){
     const squares = document.querySelectorAll('.square');
     squares.forEach(square => {
         square.style.backgroundColor="white";
+        square.id="100";
     });
 }
 function clearAll(){
-    const outerContainer = document.querySelector("#container");
     const container = document.querySelectorAll('.row');
     container.forEach(c => {
         outerContainer.removeChild(c);
@@ -47,7 +71,7 @@ size.addEventListener("click", () => {
         gridSize = prompt("Enter a valid grid size between 1 to 100", "Note: Your drawing will be erased");
     }
     reset();
-    sizeNum.textContent=`Size of Grid: ${gridSize}`;
+    sizeNum.textContent=`Grid Pixels: ${gridSize}`;
     clearAll();
     createGrid(gridSize);
     changeColor();
